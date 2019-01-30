59efff13e2731b801daf86294406b9bc3b01ca47
ManuelOS <manuelos.bubu@gmail.com>
2019-01-30 03:55:18 -0600

    holis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='e153b5cf68f0ce85f684ced65453066a'
 
 posts = [
     {
@@ -27,6 +30,24 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'bubu@gmail.com' and form.password.data == 'password':
+            flash('You Have Been Logged In!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful, please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
